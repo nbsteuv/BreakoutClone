c43f1a32a42c181c7403f5b81fdaca008db2de52
nbsteuv <nbsteuv@gmail.com>
2017-01-07 09:47:33 -0500

    Ball launches on space press

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -6,11 +6,15 @@ public class BallScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        //GetComponent<Rigidbody>().AddForce(100f, 300f, 0);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //Fire the ball
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody>().AddForce(100f, 300f, 0);
+        }
+    }
 }
