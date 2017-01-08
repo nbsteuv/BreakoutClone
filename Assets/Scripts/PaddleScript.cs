6c792b7ee8f0b273ac98d212a3b2aa5be7fd0723
nbsteuv <nbsteuv@gmail.com>
2017-01-08 18:38:00 -0500

    Don't destroy paddle on load

--- Assets/Scripts/PaddleScript.cs
+++ Assets/Scripts/PaddleScript.cs
@@ -15,6 +15,7 @@ public class PaddleScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        DontDestroyOnLoad(gameObject);
         livesText = GameObject.Find("Lives Counter").GetComponent<TextMesh>();
         SpawnBall();
 	}
