f9e8d32d675c07e56817de30760d617191668d60
nbsteuv <nbsteuv@gmail.com>
2017-01-12 06:47:05 -0500

    Added background music through MusicManager

---
+++ Assets/Scripts/MusicManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManagerScript : MonoBehaviour {
+
+    public AudioClip[] songs;
+    AudioSource currentAudio;
+
+	// Use this for initialization
+	void Start () {
+        currentAudio = GetComponent<AudioSource>();
+        currentAudio.clip = songs[0];
+        currentAudio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
