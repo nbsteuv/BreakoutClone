8413c6e3839e1aef2e31a347cdb972b1800fb247
nbsteuv <nbsteuv@gmail.com>
2017-05-10 07:43:21 -0400

    Fixed bug: music manager duplicates on restart

--- Assets/Scripts/MusicManagerScript.cs
+++ Assets/Scripts/MusicManagerScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MusicManagerScript : MonoBehaviour {
 
-    MusicManagerScript instance;
+    static MusicManagerScript instance;
 
     public AudioClip[] songs;
     AudioSource currentAudio;
